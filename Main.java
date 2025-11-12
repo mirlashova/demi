35b97113a06bd79dbac9eb926cef61405a8c67e4
mirlashova <ademimirlanova27@gmail.com>
2025-11-12 08:42:23 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package org.example;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+   int bame =2121;
+    int [] array = new int[5];
+    //[12,58,23,1,0]
+    array [0] =12;
+    array [1] =58;
+    array [2] =23;
+    array [3] =1;
+    array [4] =75;
+    System.out.println(array[1]);
+    }
+}
